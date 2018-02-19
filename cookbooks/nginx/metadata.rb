669410b200e0f6f7d30cc1bb9a043bbbd5de6958
Clivern <hello@clivern.com>
2018-02-20 00:29:33 +0100

    Simple cookbook to install nginx

---
+++ cookbooks/nginx/metadata.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+name 'nginx'
+description 'nginx cookbook'
+maintainer 'clivern'
+maintainer_email 'hello@clivern.com'
+license 'Apache-2.0'
+version '1.0.0'
+
+depends "apt-update"
